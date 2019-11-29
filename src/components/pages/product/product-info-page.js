16087a909f0a6ec22f32a55df3c8549e6ad5a359
Alexsander <alexsander11115@gmail.com>
2019-11-29 10:28:37 +0200

    Change product-info-page to class

--- src/components/pages/product/product-info-page.js
+++ src/components/pages/product/product-info-page.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component} from 'react';
 import {MDBBtn, MDBCard, MDBCol, MDBContainer, MDBIcon, MDBListGroup, MDBListGroupItem, MDBRow,} from "mdbreact";
 import {Link, Redirect, withRouter} from 'react-router-dom';
 import {authService} from "../../../shared/auth-service";
@@ -7,96 +7,102 @@ import fun from '../../../lib/function'
 import Header from "../../header";
 import "./product-page.css";
 
-const ProductPageInfo = ({match}) => {
 
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-        const fetchItems = async () => {
-
-            await authService.checkAuthTokenTime();
-            await productService.getProduct(match)
-                .then(
-                items=>
-                {
-                    setItems(items)
-                }
-
-            );
-
-        };
-        fetchItems();
-    }, [match]);
-
-    const auth_token = fun.getItem('auth_token');
-
-
-
-    if (auth_token === 'null' || auth_token === null) {
-        return (
-            <Redirect to="/login"/>
-        )
-    }
-
-    const createMarkup = (html) => {
-        return {__html: html}
+export default withRouter(class ProductPageInfo extends Component{
+    state = {
+        items: [],
+        attr: []
     };
 
-    return (
-        <div className="box-page">
-            <Header/>
-            <MDBContainer>
-                {
-                    <div>
+    componentDidMount() {
 
-                <MDBRow>
-                    <MDBCol md="6">
-                        {items.thumb}
-                    </MDBCol>
-                    <MDBCol md="6">
-                        <div>
-                            <h2>{items.name}</h2>
-                            <br/>
-                            <div></div>
-                            <div>
-                                <MDBRow>
-                                    <MDBCol md="12">
-                                        {items.attr ?
-                                            <MDBListGroup>{
-                                            items.attr.map((r) => (
+        const fetchItems = async () => {
 
-                                                    <MDBListGroupItem key={r.attr_id}>{r.name}:{r.text}</MDBListGroupItem>
+                    await authService.checkAuthTokenTime();
+                    await productService.getProduct(this.props.match)
+                        .then(
+                        items=>
+                        {
 
-                                            ))}
-                                                <MDBListGroupItem key="price">Цена:{items.price}</MDBListGroupItem>
-                                            </MDBListGroup>: ''
-                                        }
-                                    </MDBCol>
-                                </MDBRow>
-                                <Link to="/products">
-                                    <MDBBtn>
-                                        <MDBIcon icon="angle-double-left"/>
-                                        Назад
-                                    </MDBBtn>
-                                </Link>
-                            </div>
-                        </div>
-                    </MDBCol>
+                            this.setState({
+                                items: items,
+                                attr: items.attr
+                            })
 
-                </MDBRow>
+                        }
 
-                <MDBCard className={"description"}>
+                    );
+
+                };
+                fetchItems();
+    }
+
+
+    render() {
+
+        const auth_token = fun.getItem('auth_token');
+
+        if (auth_token === 'null' || auth_token === null) {
+            return (
+                <Redirect to="/login"/>
+            )
+        }
+
+        const createMarkup = (html) => {
+            return {__html: html}
+        };
+        const {items,attr} = this.state;
+        return (
+            <div className="box-page">
+                <Header/>
+                <MDBContainer>
                     {
-                        <div dangerouslySetInnerHTML={createMarkup(items.description)}/>
+                        <div>
 
+                            <MDBRow>
+                                <MDBCol md="6">
+                                    {items.thumb}
+                                </MDBCol>
+                                <MDBCol md="6">
+                                    <div>
+                                        <h2>{items.name}</h2>
+                                        <br/>
+                                        <div>
+                                            <MDBRow>
+                                                <MDBCol md="12">
+                                                        <MDBListGroup>{
+                                                            attr.map((r) => (
+
+                                                                <MDBListGroupItem key={r.attr_id}>{r.name}:{r.text}</MDBListGroupItem>
+
+                                                            ))}
+                                                            <MDBListGroupItem key="price">Цена:{items.price}</MDBListGroupItem>
+                                                        </MDBListGroup>
+                                                </MDBCol>
+                                            </MDBRow>
+                                            <Link to="/products">
+                                                <MDBBtn>
+                                                    <MDBIcon icon="angle-double-left"/>
+                                                    Назад
+                                                </MDBBtn>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </MDBCol>
+
+                            </MDBRow>
+
+                            <MDBCard className={"description"}>
+                                {
+                                    <div dangerouslySetInnerHTML={createMarkup(items.description)}/>
+
+                                }
+                            </MDBCard>
+                        </div>
                     }
-                </MDBCard>
-                    </div>
-                    }
 
-            </MDBContainer>
-        </div>
-    );
-};
-
-export default withRouter(ProductPageInfo);
+                </MDBContainer>
+            </div>
+        );
+    }
+})
